0a782c37137571c1c456720224c4227b87f202d1
Vitaly Koval <darband018@gmail.com>
2020-06-15 14:34:01 +0300

    add slick-slider

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+$('.header-slider').slick({
+        arrows: false,
+        vertical: true,
+        dots: true,
+        dotsClass: 'header-dots',
+});
+
+ $('.menu__btn').on('click', function(){
+           $('.menu__list').slideToggle();
+   }); 
+});
